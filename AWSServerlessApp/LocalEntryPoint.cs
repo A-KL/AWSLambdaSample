3fcdae9b889da09e8068cf1c01641a968af97d75
Anatolii Klots <Anatolii.Klots@albumprinter.com>
2017-05-11 10:29:00 +0200

    Initial project

---
+++ AWSServerlessApp/LocalEntryPoint.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AWSServerlessApp
+{
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// The Main function can be used to run the ASP.NET Core application locally using the Kestrel webserver.
+    /// </summary>
+    public class LocalEntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
